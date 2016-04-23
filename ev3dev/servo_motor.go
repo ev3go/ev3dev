41e41b4b6ecf1d12e73da2159f69c3542a9f49a8
kortschak <dan.kortschak@adelaide.edu.au>
2016-04-23 21:49:27 +0930

    all: assign copyright to ev3go authors

--- ev3dev/servo_motor.go
+++ ev3dev/servo_motor.go
@@ -1,4 +1,4 @@
-// Copyright ©2016 Dan Kortschak. All rights reserved.
+// Copyright ©2016 The ev3go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
