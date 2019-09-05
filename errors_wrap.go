28965374c0e41681cb0d6ec618acb0dca4a345ea
Dan Kortschak <dan@kortschak.io>
2019-09-05 18:30:46 +0930

    ev3dev: update for go1.13

---
+++ errors_wrap.go
@@ -0,0 +1,11 @@
+// Copyright ©2019 The ev3go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build go1.13
+
+// TODO(kortschak): Remove this when go1.12 no longer supported.
+
+package ev3dev
+
+func wrapped(format string) string { return format }
