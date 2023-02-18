3941386e4890f502f0e9792d41b811cbdd07e793
Dan Kortschak <dan@kortschak.io>
2023-02-19 08:38:34 +1030

    mod: update github.com/ev3go/sisyphus

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.13
 require (
 	bazil.org/fuse v0.0.0-20200117225306-7b5117fecadc
 	github.com/ev3go/ev3 v0.0.0-20180809222216-71c98163af71
-	github.com/ev3go/sisyphus v0.0.0-20200309125033-64cef7b345e9
-	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527
+	github.com/ev3go/sisyphus v0.0.0-20230218220525-74fbc3041f9b
+	golang.org/x/sys v0.5.0
 	periph.io/x/periph v3.4.0+incompatible
 )
---
+++ test.go
@@ -0,0 +1,123 @@
+// Copyright 2018 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+func main() {
+	log.SetPrefix("ci: ")
+	log.SetFlags(0)
+
+	start := time.Now()
+	defer func() {
+		log.Printf("elapsed time: %v\n", time.Since(start))
+	}()
+
+	var (
+		race    = flag.Bool("race", false, "enable race detector")
+		cover   = flag.String("coverpkg", "", "apply coverage analysis in each test to packages matching the patterns.")
+		tags    = flag.String("tags", "", "build tags")
+		verbose = flag.Bool("v", false, "enable verbose output")
+	)
+
+	flag.Parse()
+
+	pkgs, err := pkgList()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	args := []string{"test"}
+
+	if *verbose || *cover != "" || *race {
+		args = append(args, "-v")
+	}
+
+	var f *os.File
+	if *cover != "" {
+		f, err = os.Create("coverage.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		args = append(args, "-coverprofile=profile.out", "-covermode=atomic", "-coverpkg="+*cover)
+	}
+
+	if *tags != "" {
+		args = append(args, "-tags="+*tags)
+	}
+
+	switch {
+	case *race:
+		args = append(args, "-race", "-timeout=20m")
+	default:
+		args = append(args, "-timeout=10m")
+	}
+	args = append(args, "")
+
+	for _, pkg := range pkgs {
+		args[len(args)-1] = pkg
+		fmt.Println("go", args)
+		cmd := exec.Command("go", args...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if *cover != "" {
+			profile, err := ioutil.ReadFile("profile.out")
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = f.Write(profile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			os.Remove("profile.out")
+		}
+	}
+}
+
+func pkgList() ([]string, error) {
+	out := new(bytes.Buffer)
+	cmd := exec.Command("go", "list", "./...")
+	cmd.Stdout = out
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	err := cmd.Run()
+	if err != nil {
+		return nil, errors.Wrapf(err, "could not get package list")
+	}
+
+	var pkgs []string
+	scan := bufio.NewScanner(out)
+	for scan.Scan() {
+		pkg := scan.Text()
+		if strings.Contains(pkg, "vendor") {
+			continue
+		}
+		pkgs = append(pkgs, pkg)
+	}
+
+	return pkgs, nil
+}
